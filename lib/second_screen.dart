2abff554b1d7a6a4367f0eeaa827cce972393403
Şevval Özdamar <ozdamarsevval.01@gmail.com>
2023-04-08 01:25:43 +0300

    second screen bitti

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,182 +1,173 @@
-import 'package:f58_app_jam_project/second_screen.dart';
-import 'package:f58_app_jam_project/third_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      initialRoute: "/",
-      routes: {
-        "/home_screen": (context) => HomePage(),
-        "/second_screen": (context) => SecondScreen(),
-        "/third_screen": (context) => ThirdScreen(), 
-      },
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 114, 207, 240),
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text("DERS SEÇME"),
+          title: Text("Ders İçerikleri"),
           centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 211, 25, 25),
         ),
-        body: Center(child: HomePage()),
-      ),
-    ),
-  );
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-1",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-2",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-3",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-4",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-5",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-6",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-7",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+            width: 300,
+            height: 70,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/third_screen");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // yeşil arka plan rengi
+              ),
+              child: Text(
+                "MODUL-8",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+            ),
+          ),
+        ),
+      );
 }
-
-/*class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}*/
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "FLUTTER",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "UNITY",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "OYUN SANATI",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "PROJE YÖNETİMİ",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "GİRİŞİMCİLİK",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "İNGİLİZCE",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 180,
-            height: 70,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/second_screen");
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green, // yeşil arka plan rengi
-              ),
-              child: Text(
-                "KURULUMLAR",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-
